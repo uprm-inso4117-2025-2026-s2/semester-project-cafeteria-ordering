3c4ee4a903a294469dac07f2fbab97c2630f9cb6
Samarys <samarys.barreiro@upr.edu>
2026-04-03 05:23:01 -0400

    Added LTT for load&performance regarding payment screen.

---
+++ documentation/LectureTopicTasks/load&performance-testing/payment_screen_tests.js
@@ -0,0 +1,44 @@
+{
+  "generated_at": "2026-04-03T09:01:56.302Z",
+  "results": [
+    {
+      "scenario": "Performance Test",
+      "total_runs": 10,
+      "successful_runs": 10,
+      "failures": 0,
+      "average_response_time_ms": 493.33296,
+      "p95_response_time_ms": 601.9054470000001,
+      "min_response_time_ms": 288.8279709999997,
+      "max_response_time_ms": 601.9054470000001,
+      "throughput_req_per_sec": 1.3905101101189619,
+      "failure_rate": 0,
+      "success_rate": 1
+    },
+    {
+      "scenario": "Load Test",
+      "total_runs": 129,
+      "successful_runs": 128,
+      "failures": 1,
+      "average_response_time_ms": 412.7009705581398,
+      "p95_response_time_ms": 576.0277310000001,
+      "min_response_time_ms": 207.88333899999998,
+      "max_response_time_ms": 595.8410210000002,
+      "throughput_req_per_sec": 16.125,
+      "failure_rate": 0.007751937984496124,
+      "success_rate": 0.9922480620155039
+    },
+    {
+      "scenario": "Stress Test",
+      "total_runs": 376,
+      "successful_runs": 367,
+      "failures": 9,
+      "average_response_time_ms": 411.11859279787245,
+      "p95_response_time_ms": 590.2533579999981,
+      "min_response_time_ms": 202.07079599999997,
+      "max_response_time_ms": 605.2101189999976,
+      "throughput_req_per_sec": 47,
+      "failure_rate": 0.023936170212765957,
+      "success_rate": 0.976063829787234
+    }
+  ]
+}
