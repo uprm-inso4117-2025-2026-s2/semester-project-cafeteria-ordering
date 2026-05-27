17b3842b3a93f7122728e5b3d9e1803733d9750e
YandreCaban <123414524+YandreCaban@users.noreply.github.com>
2026-05-27 16:11:44 -0400

    Metro.config.js Fix

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,4 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
@@ -9,4 +8,4 @@ config.resolver.blockList = [
     /node_modules\/@stripe\/stripe-react-native\/.*/,
 ];
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = config;
